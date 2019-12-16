bbc94b98ce4c625544642bcf43fae9dab63ce173
Auri <ainaa.105@gmail.com>
2019-12-16 10:09:07 +0100

    sobrava un corchete

--- src/views/EditIssue.vue
+++ src/views/EditIssue.vue
@@ -2,42 +2,21 @@
   <div class="container mt-4">
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
       <b-form-group id="input-group-1" label="Títol:" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="form.titol"
-          required
-          placeholder="Anomeneu l'issue"
-        ></b-form-input>
+        <b-form-input id="input-1" v-model="form.titol" required placeholder="Anomeneu l'issue"></b-form-input>
       </b-form-group>
 
       <b-form-group id="input-group-2" label="Descripció:" label-for="input-2">
-        <b-form-textarea
-          id="textarea"
-          v-model="form.descripcio"
-          rows="3"
-          max-rows="6"
-        ></b-form-textarea>
+        <b-form-textarea id="textarea" v-model="form.descripcio" rows="3" max-rows="6"></b-form-textarea>
 
         <pre class="mt-3 mb-0">{{ text }}</pre>
       </b-form-group>
 
       <b-form-group id="input-group-3" label="Tipus:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.tipus"
-          :options="tipus"
-          required
-        ></b-form-select>
+        <b-form-select id="input-3" v-model="form.tipus" :options="tipus" required></b-form-select>
       </b-form-group>
       <b-form-group id="input-group-3" label="Prioritat:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.prioritat"
-          :options="prioritat"
-          required
-        ></b-form-select>
+        <b-form-select id="input-3" v-model="form.prioritat" :options="prioritat" required></b-form-select>
       </b-form-group>
-
 
       <b-button type="submit" variant="primary">Crea l'issue</b-button>
       <b-button type="reset" variant="danger">Descartar</b-button>
@@ -49,38 +28,50 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        form: {
-          titol: '',
-          descripcio: '',
-          tipus: null,
-          prioritat: null
-        },
-        prioritat: [{text: "Tria'n una", value: null}, 'Trivial', 'Menor', 'Major', 'Crítica', 'Bloquejant'],
-        tipus: [{ text: "Tria'n un", value: null }, 'Bug', 'Millora', 'Proposta', 'Tasca'],
-        show: true
-      }
-    },
-    methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
+export default {
+  data() {
+    return {
+      form: {
+        titol: "",
+        descripcio: "",
+        tipus: null,
+        prioritat: null
       },
-      onReset(evt) {
-        evt.preventDefault()
-        // Reset our form values
-        this.form.titol = ''
-        this.form.descripcio = ''
-        this.form.tipus = null
-        this.form.prioritat = null
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
-      }
+      prioritat: [
+        { text: "Tria'n una", value: null },
+        "Trivial",
+        "Menor",
+        "Major",
+        "Crítica",
+        "Bloquejant"
+      ],
+      tipus: [
+        { text: "Tria'n un", value: null },
+        "Bug",
+        "Millora",
+        "Proposta",
+        "Tasca"
+      ],
+      show: true
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.titol = "";
+      this.form.descripcio = "";
+      this.form.tipus = null;
+      this.form.prioritat = null;
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     }
   }
 };
